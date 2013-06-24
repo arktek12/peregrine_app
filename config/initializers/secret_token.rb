c0f0bd925ec644a63a6a9a658199bec5adff6bb8
admin <vjyourtech@gmail.com>
2013-06-24 22:20:00 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeregrineApp::Application.config.secret_token = '272e8ac967bfda6f4c618ac761175cfab2f31ddfa3cad093215793e855cc9f04caee5ad6bfdc8debf5f8cd378b804f35bc1fd4ddc1b97fe784a0f89945b988b6'
